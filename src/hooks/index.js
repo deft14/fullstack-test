6449da87c236212311ec0ae93aedbf16d1e9ffc5
YanPuty <yanputy123@gmail.com>
2022-10-10 07:20:03 +0700

    enhancement

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -32,3 +32,36 @@ export function useApi(service, params = {}, effects = []) {
     error,
   };
 }
+
+export function useApi2(service, params = {}, effects = []) {
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    service(
+      "https://603c67f9f4333a0017b6786c.mockapi.io/api/v1/Products",
+      params,
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setResponse(res);
+        setLoading(false);
+        setLoaded(true);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+        setLoaded(true);
+      });
+    return () => {};
+  }, effects);
+
+  return {
+    loading,
+    loaded,
+    response,
+    error,
+  };
+}
--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,30 +1,118 @@
 import React, { useEffect, useState } from "react";
 import { useApi } from "../../hooks";
-import { Button } from "antd";
-import { Link } from "react-router-dom";
+import { Input, Select } from "antd";
 
 // Components
-import TableComponent from "./components/index";
+import { MenuOutlined, SearchOutlined, StarFilled } from "@ant-design/icons";
+const { Option } = Select;
 
 function ListComponent() {
+  const filter = [
+    {
+      name: "Food",
+    },
+    {
+      name: "Beverage",
+    },
+    {
+      name: "Desserts",
+    },
+    {
+      name: "Wine Lists",
+    },
+    {
+      name: "Pizza",
+    },
+    {
+      name: "Burger",
+    },
+  ];
   const [products, setProducts] = useState([]);
   const { response, loading } = useApi(fetch);
 
   useEffect(() => {
-    setProducts(response);
+    handlingResponse();
   }, [response]);
 
+  const handlingResponse = () => {
+    const products = response || [];
+    setProducts(products.slice(0, 10));
+  };
+
+  const handlingFilter = (e) => {
+    const name = e.target.value;
+    if (name) {
+      setProducts(
+        products.filter((pro) => {
+          return pro?.name?.indexOf(name) > -1;
+        }),
+      );
+    } else {
+      setProducts(response);
+    }
+  };
+
   return (
-    <div>
-      <h2 style={{ textAlign: "center" }}>List of Products</h2>
-      <div style={{ textAlign: "center", marginBottom: "10px" }}>
-        <Button type="primary" size="small">
-          <Link to="/create">Create Product</Link>
-        </Button>
+    <div className="container">
+      <div className="filter-container">
+        <div>ICON</div>
+        <Input
+          prefix={<SearchOutlined />}
+          placeholder="Search"
+          onChange={handlingFilter}
+          style={{ borderRadius: "5px" }}
+        />
+        <MenuOutlined />
       </div>
-      <div className="table-products">
-        <TableComponent child={products} loading={loading} />
+      <div className="flex-row mt-30 gap-10">
+        <Select defaultValue="lucy" style={{ width: 120, borderRadius: "5px" }}>
+          <Option value="jack">Jack</Option>
+          <Option value="lucy">Lucy</Option>
+          <Option value="disabled" disabled>
+            Disabled
+          </Option>
+          <Option value="Yiminghe">yiminghe</Option>
+        </Select>
+        <div className="flex-row gap-10" style={{ overflow: "scroll" }}>
+          {filter.map((item, index) => (
+            <div className="item" key={index}>
+              {item.name}
+            </div>
+          ))}
+        </div>
       </div>
+
+      {products &&
+        products.map((item, index) => (
+          <div className="card-wrapper" key={index}>
+            <div className="card-left-side">
+              <div className="header">
+                <div className="name">{item.name}</div>
+                <div className="price">{"$ " + item.price}</div>
+              </div>
+              <div>
+                <span className="mr-4">4.0</span>
+                <StarFilled style={{ color: "#FF9529" }} />
+              </div>
+              <div>{item.description}</div>
+            </div>
+            <div className="card-right-side">
+              <div className="plus-sign"> + </div>
+              <img
+                className="image"
+                src={item.img}
+                imag
+                onError={(e) =>
+                  e.target.onerror === null
+                    ? (e.target.src =
+                        "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png")
+                    : ""
+                }
+                alt={item.img}
+              />
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
